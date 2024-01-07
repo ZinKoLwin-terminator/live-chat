85165d20af6b5f4e8442c9aad432bf06f099ebfb
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-07 19:56:54 +0630

    toggle two component and firebase auth setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-
+import "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDKfkEtOo9kItqi6CAHFG7X52HJA_7NPfg",
@@ -13,6 +13,7 @@ const firebaseConfig = {
   
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
+let auth = firebase.auth();
 let timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { db, timestamp };
+export { db, timestamp,auth };
