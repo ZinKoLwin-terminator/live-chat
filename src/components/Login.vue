3b2af44d0b5fb573859d8239e59376ea639cdcda
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-07 23:49:37 +0630

    let's build our login system

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,25 +3,31 @@
     <form @submit.prevent="login">
       <input type="email" placeholder="email" v-model="email">
       <input type="password" placeholder="password" v-model="password">
+      <div class="error" v-if="error">{{error}}</div>
       <button>Login</button>
     </form>
   </template>
   
   <script>
   import { ref } from 'vue'
-  export default {
-      setup() {
-         
-          let email = ref("");
-          let password = ref("");
-  
-          let login = () => {
-              console.log(email.value, password.value);
-          }
-  
-          return { email, password ,login};
-  }
-  }
+import {auth} from "../firebase/config"
+import useLogin from "../composables/useLogin"
+export default {
+    setup(){
+        
+        let email=ref("");
+        let password=ref("");
+       
+        let {error,signIn}= useLogin();
+        let login=async()=>{
+           let res=await  signIn(email.value,password.value)
+           if(res){
+               console.log(res.user);
+           }
+        }
+        return {email,password,login,error};
+    }
+}
   </script>
   
   <style>
