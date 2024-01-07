f0cae6d3f3c71c2e8bdb1165dc5d2fbdb5d2b064
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-07 20:32:13 +0630

    sign up user

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Sign Up</h1>
+  
   <form @submit.prevent="signUP">
     <input type="text" placeholder="display name" v-model="displayName">
     <input type="email" placeholder="email" v-model="email">
@@ -10,14 +11,24 @@
 
 <script>
 import { ref } from 'vue'
+import { auth } from "../firebase/config";
 export default {
     setup() {
         let displayName = ref("");
         let email = ref("");
         let password = ref("");
+        let error = ref(null);
 
-        let signUP = () => {
-            console.log(displayName.value, email.value, password.value);
+        let signUP =async () => {
+           try {
+            let resp = await auth.createUserWithEmailAndPassword(email.value, password.value);
+               if (!resp) {
+                   throw new Error("could not create new user");
+            }
+           } catch (err) {
+               error.value = err.message;
+               console.log(error.value);
+           }
         }
 
         return { displayName, email, password ,signUP};
