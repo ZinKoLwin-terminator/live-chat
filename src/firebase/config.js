81d75e229a69cc2fdee5861c39bd89d25c76484b
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-07 19:31:27 +0630

    firebase-setup

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKfkEtOo9kItqi6CAHFG7X52HJA_7NPfg",
+    authDomain: "vue-blog-system-6189e.firebaseapp.com",
+    projectId: "vue-blog-system-6189e",
+    storageBucket: "vue-blog-system-6189e.appspot.com",
+    messagingSenderId: "353502369248",
+    appId: "1:353502369248:web:c870763020452692c0c785"
+};
+  
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, timestamp };
