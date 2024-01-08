7c84d06c676fe1ee517b4000b61abfd1253d9c8a
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-08 10:09:23 +0630

    create navbae component and make logout  button work

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <div>
+        <p>Hi display-name</p>
+        <p class="email">logged in as email</p>
+    </div>
+    <button @click="logout">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { auth } from "../firebase/config";
+export default {
+    setup() {
+        let error = ref(null);
+        let logout = async () => {
+            try {
+                await auth.signOut();
+            console.log("user loggedd out");
+            } catch (err) {
+                error.value = err.message;
+                console.log(error.value);
+            }
+        }
+
+        return { logout };
+}
+}
+</script>
+
+<style>
+
+</style>
