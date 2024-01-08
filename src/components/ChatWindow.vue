a34fe456cdc373baf08c93a9d273d9dde769573e
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-01-09 03:18:29 +0630

    auto scrolling feature

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-window">
-    <div class="messages" >
+    <div class="messages" ref="msgBox">
         <div class="single" v-for="message in formatedMessages" :key="message.id">
             <span class="created-at">{{message.created_at}}</span>
             <span class="name">{{message.name}}</span>
@@ -11,12 +11,19 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, onUpdated, ref } from 'vue';
 import { db } from "../firebase/config";
 import { formatDistanceToNow } from "date-fns";
 export default {
     setup() {
         let messages = ref([]);
+        let msgBox = ref(null);
+
+        //auto scrolling feature
+
+        onUpdated(() => {
+            msgBox.value.scrollTop = msgBox.value.scrollHeight
+        })
         let formatedMessages = computed(() => {
             return messages.value.map((message) => {
                 let formatTime = formatDistanceToNow(message.created_at.toDate());
@@ -35,7 +42,7 @@ export default {
             })
             messages.value = results;
         })
-        return { messages,formatedMessages };
+        return { messages,formatedMessages,msgBox };
 }
 }
 </script>
